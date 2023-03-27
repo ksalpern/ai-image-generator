97c209512032638db6248159c3ea4b5f34c4a3d7
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-03-27 22:58:26 +0300

    add react-hot-toast provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import PromptInput from "../components/PromptInput";
-// import ClientProvider from "../components/ClientProvider";
+import ClientProvider from "../components/ClientProvider";
 import "../styles/globals.css";
 
 export const metadata = {
@@ -16,13 +16,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* <ClientProvider> */}
-        <Header />
+        <ClientProvider>
+          <Header />
 
-        <PromptInput />
+          <PromptInput />
 
-        {children}
-        {/* </ClientProvider> */}
+          {children}
+        </ClientProvider>
       </body>
     </html>
   );
