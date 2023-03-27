aec1599858b21cb14290da7941a315e96f0e28bf
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-03-27 19:59:25 +0300

    add openai lib to azure

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -1,27 +1,28 @@
 const { app } = require("@azure/functions");
-// const openai = require("../../lib/openai");
+const openai = require("../../lib/openai");
+// const openai = require("../../lib/fetchSuggestionFromChatGPT");
 
 app.http("getChatGPTSuggestion", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: async (request, context) => {
-  //   const response = await openai.createCompletion({
-  //     model: "text-davinci-003",
-  //     prompt:
-  //       "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
-  //     max_tokens: 100,
-  //     temperature: 0.8,
-  //   });
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt:
+        "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+      max_tokens: 100,
+      temperature: 0.8,
+    });
 
-  //   context.log(`Http function processed request for url "${request.url}"`);
+    context.log(`Http function processed request for url "${request.url}"`);
 
-  //   const responseText = response.data.choices[0].text;
+    const responseText = response.data.choices[0].text;
 
-    // return {
-    //   body: responseText,
-    // };
-      return {
-      body: `hello you!`,
+    return {
+      body: responseText,
     };
+    //   return {
+    //   body: `hello you!`,
+    // };
   },
 });
